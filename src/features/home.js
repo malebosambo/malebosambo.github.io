a8b6e2765d7296cd5a5fa6f6d3eec718dc3aa4c7
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-06-12 10:31:53 +0000

    update design mobile

--- src/features/home.js
+++ src/features/home.js
@@ -25,24 +25,33 @@ export default function Home() {
   return (
     <div className="Container">
 
-      <div className="CV-Overview">
+      <div className="Overview">
         <div className="Avatar">
           <img src="./CVAvatar.jpg" alt="avatar" className="img-circle" />
         </div>
-        <div className="Basic-Details">
+        <div className="Profile">
           <h1>Malebo Sambo</h1><br />
           <h3>Software Developer/Engineer, Entrepreneur, Business Analyst</h3><br />
           <h5>Email: sambo.malebo96@gmail.com</h5><br />
           <h5>Cell: +27630013711</h5><br />
           <h5>Location: JHB/CPT/Remote</h5>
         </div>
-      </div>
- 
-      <div className="container">
-        <div className="row">
-          {services.map((service) => <ContentItem key={service.id} image={service.imgUrl} name={service.name} list={service.ops} link={service.url} />)}
+        <div className="Online">
+          <a href="www.malebosambo.github.app"><img src="" alt="portfolio" /></a>
+          <a href="www.linkedin.com/malebosambo"><img src="" alt="LinkedIn" /></a>
+          <a href="www.github.com/sambomalebo"><img src="" alt="GitHub" /></a>
+          <a href="www.twitter.com/lovais000"><img src="" alt="Twitter" /></a>
         </div>
       </div>
+ 
+      <div className="Content">
+      <div style={{paddingBottom: "30px", textAlign: "center"}}><h1>Services</h1></div>
+        <div className="container">
+          <div className="row">
+            {services.map((service) => <ContentItem key={service.id} image={service.imgUrl} name={service.name} list={service.ops} link={service.url} />)}
+          </div>
+        </div>
+      </div>  
 
       <div className="Repo-List">
         <div style={{paddingBottom: "30px", textAlign: "center"}}><h1>Repositories</h1></div>
