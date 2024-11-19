b0e29c040efd66fa50d3ff5b9f76d39368e43cb2
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2024-11-19 12:46:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express();
 const port = 4000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static("public"));
 
@@ -24,5 +24,4 @@ app.get("/view-repo", async (req, res) => {
   res.json({ "message": "OK" });
 });
 
-
-app.listen(port, console.log(`Server started on ${port}`));
+app.listen(port, () => {console.log(`Server listening on ${port}`)});
