9648f2e10098b5fbcb05135efdc2b3c9babecac0
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-01-03 11:22:17 +0200

    Update issueTable.js

--- src/features/issueTable.js
+++ src/features/issueTable.js
@@ -1,54 +1,38 @@
-import React from 'react';
-import { useRequestAPI } from '../security/useRequestAPI';
+import React, { useState, useEffect } from 'react';
 import IssueItem from './issueItem';
 
+export default function IssueTable({ repoName }) {
 
-export default function IssueTable() {
-
-  const { data, isLoaded, error } = useRequestAPI(
-    'https://api.github.com/repos/malebosambo/nava/issues'
-  );
-
-  /*
-  const [issues,setIssues] = useState(issues);
+  const [ issues, setIssues ] = useState([]);
 
   useEffect(() => {
     const fetchRepoIssues = async (username, reponame) => {
-
       const response = await fetch(`https://api.github.com/repos/${username}/${reponame}/issues`);
 
-      const issueData = await response.json();
-
-      setIssues(issueData);
+      const data = await response.json();
+      setIssues(data);
     };
 
-    fetchRepoIssues('malebosambo', 'portfolio');
+    fetchRepoIssues('malebosambo', repoName);
   }, []); 
-  */
-  
-  if (error) {
-    return <div>Error: {error}</div>;
-  } else if (isLoaded) {
-    return <div>Loading...</div>;
-  } else {
+
     return (
-      <>
-        <div className="TableResponse">
-          <table>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Created Date</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((issue) => <IssueItem key={issue.id} title={issue.title} description={issue.body} date={issue.created_at} status={issue.state} />)}
-            </tbody>
-          </table>
-        </div>
-      </>
-    )
-  }
+    <>
+      <div className="TableResponse">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Created Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {issues.map((issue) => <IssueItem key={issue.id} title={issue.title} description={issue.body} date={issue.created_at} status={issue.state} />)}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
 }
