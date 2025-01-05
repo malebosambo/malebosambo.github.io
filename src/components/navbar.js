678b7e06f5a57d28426c16cd80de491d4157fde1
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-01-05 09:37:11 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,6 @@ export default function Navbar() {
       <Link to="/"><h6>Home</h6></Link>
       <Link to="/cv-resume"><h6>CV</h6></Link>
       <Link to="/repos"><h6>Repos</h6></Link>
-      <Link to="/issues"><h6>Issues</h6></Link>
       <Link to="/contacts"><h6>Contacts</h6></Link>
     </>       
   )
