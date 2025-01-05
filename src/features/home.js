1ed5260d647873ea5bebd13d67d26b43aa5aa0ef
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-01-05 09:01:08 +0200

    Update home.js

--- src/features/home.js
+++ src/features/home.js
@@ -44,25 +44,27 @@ export default function Home() {
           <img src="./Malebo-Sambo.png" alt="Avatar" className="img-fluid" style={{ borderRadius: 50 }} />
         </div>
         <div className="Profile">
-          <br />
-          <h1>Malebo Sambo</h1><br />
-          <h5>Founder, CEO, CTO, Business Analyst</h5><br />
-          <img src="Email.png" alt="Email Icon" className="Icon" />
-          <a href="mailto:sambo.malebo96@gmail.com"><p>sambo.malebo96@gmail.com</p></a><br />
-          <img src="./Telephone.png" alt="Telephone Icon" className="Icon" />
-          <a href="tel:+27630013711"><p>+27630013711</p></a><br />
-          <img src="./Location.png" alt="Location Icon" className="Icon" />
-          <p>South Africa</p><br />
-        </div>
-        <div className="Online">
-          <a href="www.linkedin.com/malebosambo"><img src="./Linkedin.png" alt="LinkedIn" className="Icon" /></a>
-          <a href="www.github.com/sambomalebo"><img src="./GitHub.png" alt="GitHub" className="Icon" /></a>
-          <a href="www.twitter.com/lovais000"><img src="./Twitter.png" alt="Twitter" className="Icon" /></a>
+          <div className="Personal">
+            <br />
+            <h1>Malebo Sambo</h1><br />
+            <h5>Founder, CEO, CTO, Business Analyst</h5><br />
+            <img src="Email.png" alt="Email Icon" className="Icon" />
+            <a href="mailto:sambo.malebo96@gmail.com"><p>sambo.malebo96@gmail.com</p></a><br />
+            <img src="./Telephone.png" alt="Telephone Icon" className="Icon" />
+            <a href="tel:+27630013711"><p>+27630013711</p></a><br />
+            <img src="./Location.png" alt="Location Icon" className="Icon" />
+            <p>South Africa</p><br />
+          </div>
+          <div className="Online">
+            <a href="www.linkedin.com/malebosambo"><img src="./Linkedin.png" alt="LinkedIn" className="Icon" /></a>
+            <a href="www.github.com/sambomalebo"><img src="./GitHub.png" alt="GitHub" className="Icon" /></a>
+            <a href="www.twitter.com/lovais000"><img src="./Twitter.png" alt="Twitter" className="Icon" /></a>
+          </div>
         </div>
       </div>
  
       <div className="Content">
-        <div style={{paddingBottom: "30px", textAlign: "center"}}><h1>Services</h1></div>
+        <div style={{paddingBottom: "30px", textAlign: "center"}}><h1>Services Offered</h1></div>
         <div className="container">
           <div className="row">
           {services.map((service) => <ContentItem key={service.id} image={service.imgUrl} name={service.name} list={service.ops} link={service.url} />)}
@@ -71,7 +73,7 @@ export default function Home() {
       </div>
 
       <div className="Repo_List">
-        <div style={{paddingBottom: "30px", textAlign: "center" }}><h1>Repositories</h1></div>
+        <div style={{paddingBottom: "30px", textAlign: "center" }}><h1>My Repos</h1></div>
         <div className="Repo_Table"><RepoTable /></div>
         <div className="Repo_Cards">{repos.map((repo) => <RepoCard name={repo.name} description={repo.description} issues={repo.open_issues_count} link={repo.url} />)}</div>
       </div> 
